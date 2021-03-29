ef556295aef08e13ab857a93dd7f83bf01d13727
Cristian Jigau [Intern] <cristian.jigau@endava.com>
2021-03-30 00:47:25 +0300

    Range Task

--- 5-range.js
+++ 5-range.js
@@ -16,10 +16,32 @@
  */
 
 // Your code here.
+const range = (start, end, optional) => {
+  let arr = [];
+  if (!optional) {
+    for (; start <= end; start++) {
+      arr.push(start);
+    }
+  } else {
+    while(start!=end+optional){
+          arr.push(start);
+          start+=optional;
+      }
+    }
+  return arr;
+};
+
+const sum = arr => {
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
+};
 //
-// console.log(range(1, 10));
+console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(5, 2, -1));
+console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10)));
 // → 55
