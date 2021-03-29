af6da82e84464c215d10778a9be69482b544eeb2
Cristian Jigau [Intern] <cristian.jigau@endava.com>
2021-03-29 23:39:42 +0300

    Code for task 2

--- 2-fiz-buzz.js
+++ 2-fiz-buzz.js
@@ -13,3 +13,32 @@
  */
 
 // Your code here.
+
+console.log('\nmethod 1:');
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz');
+  else if (i % 3 == 0) console.log('Fizz');
+  else if (i % 5 == 0) console.log('Buzz');
+  else console.log(i);
+}
+
+// method 2
+
+console.log('\nmethod 2:');
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+    continue;
+  }
+  if (i % 3 == 0) {
+    console.log('Fizz');
+    continue;
+  }
+  if (i % 5 == 0) {
+    console.log('Buzz');
+    continue;
+  }
+  console.log(i);
+}
