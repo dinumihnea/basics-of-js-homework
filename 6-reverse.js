bc09f76486eee92cb7cde3d33324b8ab588f51ad
Cristian Jigau [Intern] <cristian.jigau@endava.com>
2021-03-30 01:02:57 +0300

    Reverse Task

--- 6-reverse.js
+++ 6-reverse.js
@@ -9,5 +9,33 @@
 
 // Your code here.
 
-// console.log(reverseArray(["A", "B", "C"]));
+// Toate 3 functioneaza
+
+const reverseArray = arr => {
+  let reversed = [];
+  arr.forEach(element => {
+    reversed.unshift(element);
+  });
+  return reversed;
+};
+
+const reverseArray2 = arr => {
+  let reversed = [];
+  for (let i = 1; i <= arr.length; i++) {
+    reversed[i - 1] = arr[arr.length - i];
+  }
+  return reversed;
+};
+
+const reverseArray3 = arr => {
+  let aux;
+  for (let i = 0; i < arr.length / 2; i++) {
+    aux = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = aux;
+  }
+  return arr;
+};
+
+console.log(reverseArray(['A', 'B', 'C']));
 // → ["C", "B", "A"];
