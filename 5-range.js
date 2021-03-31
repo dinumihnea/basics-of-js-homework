ed331747d90699b5fc271386bf4b413f397f8718
dasha <misery.hatred.anger@gmail.com>
2021-03-31 13:00:11 +0300

    Fifth task

--- 5-range.js
+++ 5-range.js
@@ -16,10 +16,32 @@
  */
 
 // Your code here.
-//
-// console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
-// → 55
+
+function range(start, end, step = 1) {
+    let result = [];
+    if (start < end) {
+        for (let i = start; i <= end; i += step) {
+            result.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+console.log(range(1, 10));
+console.log(range(1, 10, 2));
+console.log(range(5, 2, -1));
+
+function sum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
+}
+
+console.log(sum(range(1, 10)))
+console.log(sum(range(5, 2, -1)))
