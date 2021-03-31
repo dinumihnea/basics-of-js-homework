33d97e9993c9a5f254900d829e18d6fe2e344a9e
Dima <dimahinev31999@gmail.com>
2021-03-31 10:13:12 +0300

    second task completed

--- 2-fiz-buzz.js
+++ 2-fiz-buzz.js
@@ -12,4 +12,10 @@
  *
  */
 
-// Your code here.
+const iterationCount = 100
+for (let i = 0; i < iterationCount; i++) {
+  if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz')
+  else if (i % 3 === 0) console.log('Fizz')
+  else if (i % 5 === 0) console.log('Buzz')
+  else console.log(i)
+}
